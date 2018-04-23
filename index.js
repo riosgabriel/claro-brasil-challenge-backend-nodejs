817d07a6d72d0fbb3b871b5173f2cdaa43ad8b23
Gabriel Rios <gabrielfrios@gmail.com>
2018-04-23 12:50:51 -0300

    Implementing the API actions and connection to handler and database layer

--- index.js
+++ index.js
@@ -1,20 +1,50 @@
 const express = require('express'),
-      logger = require('./lib/logger');
+    bodyParser = require('body-parser'),
+    logger = require('./lib/logger'),
+    handler = require('./lib/device_handler'),
+    database = require('./lib/database');
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.post('/user/device', function (req, res, next) {
-    res.send('Post Method')
+app.post('/users/:user_id/devices', function (req, res, next) {
+    console.log(req.body);
+
+    handler.create(req.body.userId, req.body.device)
+        .then(() => {
+            res.status(200).send({status: 200})
+        })
+        .catch((error) => {
+            res.status(500).send({status: 500, message: error})
+        });
 });
 
-app.put('/user/device/:id', function (req, res, next) {
-    res.send('Put Method')
+app.put('/users/:user_id/devices/:device_id', function (req, res, next) {
+    handler.update(req.params.user_id, req.params.device_id, req.body.device)
+        .then(() => {
+            res.status(200).send({status: 200})
+        })
+        .catch((error) => {
+            res.status(500).send({status: 500, message: error})
+        });
 });
 
-app.delete('/user/device/:id', function (req, res, next) {
-    res.send('Delete Method')
+app.delete('/users/:user_id/devices/:device_id', function (req, res, next) {
+    handler.delete(req.params.user_id, req.params.device_id)
+        .then(() => {
+            res.status(200).send({status: 200})
+        })
+        .catch((error) => {
+            res.status(500).send({status: 500, message: error})
+        });
 });
 
-app.listen(3000, function () {
-    logger.info('Server is listening on port 3000');
-});
+database.openConnection()
+    .then(() => {
+        console.log('Database connected.');
+
+        app.listen(3000, function () {
+            logger.info('Server is listening on port 3000');
+        });
+    });
